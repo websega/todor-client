b679cb07a14726cbd81493cd8a0df77fae51b303
Sergey Vakhramov <vsa8813@gmail.com>
2021-01-29 22:02:26 +0500

    feat(components): add the name of the selected folder to the dashboard
    When you click on a folder, display the name in the info panel.
    In case the folder is not selected
    do not show the item and return null

--- src/components/TaskInfoPanel/TaskInfoPanel.tsx
+++ src/components/TaskInfoPanel/TaskInfoPanel.tsx
@@ -4,6 +4,7 @@ import { useSelector } from 'react-redux';
 import classNames from 'classnames';
 
 import { InitialSystemStateType } from '../../redux/reducers/systemReducer';
+import { InitialFolderStateType } from '../../redux/reducers/folderReducer';
 
 import SortIconAB from '../../assets/images/icons/sort_a-z.svg';
 import DotsIcon from '../../assets/images/icons/more_dots.svg';
@@ -12,38 +13,54 @@ import TaskInfoTitle from './TaskInfoTitle';
 import IconButton from '../IconButton';
 
 import classes from './TaskInfoPanel.scss';
+import { FolderType } from '../../redux/actions/folder/types';
 
 type StateType = {
+  foldersList: InitialFolderStateType;
   system: InitialSystemStateType;
 };
 
 const TaskInfoPanel = (): JSX.Element => {
+  const folders = useSelector((state: StateType) => state.foldersList.folders);
+
+  const currentFolderId = useSelector(
+    (state: StateType) => state.system.currentFolder
+  );
+
+  const currentFolder: FolderType | undefined = folders.find(
+    (folder) => folder._id === currentFolderId
+  );
+
   const currentColor = useSelector(
     (state: StateType) => state.system.currentColor
   );
 
   return (
-    <div
-      className={classNames(
-        classes.info,
-        classes[`borderBottomColor-${currentColor}`]
-      )}
-    >
-      <TaskInfoTitle title="TaskInfoTitle" />
+    <>
+      {currentFolder ? (
+        <div
+          className={classNames(
+            classes.info,
+            classes[`borderBottomColor-${currentColor}`]
+          )}
+        >
+          <TaskInfoTitle title={currentFolder.name} />
 
-      <div className={classes.right}>
-        <IconButton
-          icon={<SortIconAB />}
-          iconType="sort"
-          onClick={() => console.log('click')}
-        />
-        <IconButton
-          icon={<DotsIcon />}
-          iconType="dots"
-          onClick={() => console.log('click')}
-        />
-      </div>
-    </div>
+          <div className={classes.right}>
+            <IconButton
+              icon={<SortIconAB />}
+              iconType="sort"
+              onClick={() => console.log('click')}
+            />
+            <IconButton
+              icon={<DotsIcon />}
+              iconType="dots"
+              onClick={() => console.log('click')}
+            />
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 };
 
