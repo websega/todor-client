a69cb7bd9ba06bfab71a1faaddbebc953b477f2e
Sergey Vakhramov <vsa8813@gmail.com>
2021-03-19 22:31:27 +0500

    build: heroku deploy

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import path from 'path';
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static(__dirname));
+app.use(express.static(path.resolve(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(PORT);
